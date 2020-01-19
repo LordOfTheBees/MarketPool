31e414c9eb7b448452c21c2a57453a01f3cd68c1
LordOfTheBees <lordofbeehive@gmail.com>
2020-01-19 22:43:30 +0300

    Rename contract

--- contracts/StorePool_SaleRelease.sol
+++ contracts/StorePool_SaleRelease.sol
@@ -2,8 +2,9 @@ pragma solidity >=0.5.0;
 
 import "./StorePool_Items.sol";
 
-contract StorePool_Sale is StorePool_Items {
+contract StorePool_SaleRelease is StorePool_Items {
     using SafeMath for uint256;
+    
     
     
 }
